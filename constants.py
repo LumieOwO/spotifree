4031cc4480bacb8aac787a3915b61733f6e78795
heylolv1 <hxhd10525@gmail.com>
2023-01-09 13:09:59 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,8 +1,8 @@
 import os
-CLIENT_SECRET = "f5989c30d4eb43669ecfa5fbbe0b76ac"
-CLIENT_ID = "e1e13a5b6e124e60b60c03d6fc85f87b"
+CLIENT_SECRET = ""
+CLIENT_ID = ""
 REDIRECT_URI = "http://localhost:8080/"
-USERNAME = "p2w0mirazt8tg3h330gbt0lti"
+USERNAME = ""
 SCOPE = "user-read-currently-playing"
 PATH = fr'{os.environ.get("APPDATA")}\Spotify\Spotify.exe'
     
